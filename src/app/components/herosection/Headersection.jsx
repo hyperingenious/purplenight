1026f5f9ebca6990cad195a8db71753b95f8450f
hyperingenious <skbmasale941@gmail.com>
2025-03-06 17:12:19 +0530

    FIX: minor fix in alignment

--- src/app/components/herosection/Headersection.jsx
+++ src/app/components/herosection/Headersection.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Headersection = () => {
   return (
     <div>
-      <section style={{ display: 'flex', flexDirection: 'column', marginTop: '0', padding: '2rem', gap: '40px', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={{ width: '100%', textAlign: 'left', flex: 1 }}>
+      <section style={{ display: 'flex', flexWrap: 'wrap', marginTop: '0', padding: '2rem', gap: '40px', alignItems: 'center', justifyContent: 'space-between' }}>
+        <div style={{  width: '100%', textAlign: 'left', flex: 1 }}>
           <h1 style={{ fontSize: '7vw', fontWeight: 'bold', marginBottom: '2.5rem' }}>PURPLENIGHT</h1>
           <p style={{ fontSize: '1.125rem', marginBottom: '1rem' }}>
             PurpleNight transforms how you consume knowledge. Using advanced AI, it distills entire books into concise, blog-style summaries, perfect for busy professionals and learners. No fluff—just the most impactful insights. In minutes, you grasp the core ideas, making PurpleNight your go-to resource for efficient, focused learning. Stay informed, save time, and absorb meaningful content effortlessly.
