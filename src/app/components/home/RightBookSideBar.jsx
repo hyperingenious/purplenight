823c5d31985a7c5653473714f144caa79b5d0a9b
hyperingenious <skbmasale941@gmail.com>
2025-03-13 06:13:18 +0530

    REMOVE & CHANGE: removed Active Card change slug handeling

--- src/app/components/home/RightBookSideBar.jsx
+++ src/app/components/home/RightBookSideBar.jsx
@@ -3,41 +3,49 @@ import { useQuery } from "@tanstack/react-query";
 import BookCards from "../bottom-bar/BookCards";
 import LoadingSkeleton from "../bottom-bar/Skeleton";
 import { ScrollArea, Stack, useComputedColorScheme } from "@mantine/core";
-import { memo, useState } from "react";
-import ActiveBookCard from "../bottom-bar/ActiveBookCard";
+import { memo, useEffect, useState } from "react";
 import { useAuth } from "@clerk/nextjs";
+import { useParams } from "next/navigation";
 
 function RightBookSidebar() {
   const colorScheme = useComputedColorScheme();
   const [activeBook, setActiveBook] = useState(null);
-  const { getToken } = useAuth()
+  const { slug } = useParams();
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    if (slug) {
+      setActiveBook(slug[0]);
+    }else(setActiveBook(null))
+  },[slug]);
 
   const {
     data: books,
     isSuccess,
     isLoading,
   } = useQuery({
-    queryKey: ["book"],
+    bqueryKey: ["book"],
     queryFn: () => fetchBook(getToken),
     cacheTime: Infinity,
   });
 
+
   return (
-    <ScrollArea
-      scrollbarSize={6}
-      h={"100vh"}
-      scrollbars="y"
-    >
+    <ScrollArea scrollbarSize={6} h={"100vh"} scrollbars="y">
       <Stack gap="xs" ml={"sm"} m="xs">
-        {activeBook && <ActiveBookCard colorScheme={colorScheme} {...activeBook} />}
+        {/* {activeBook && (
+          <ActiveBookCard colorScheme={colorScheme} {...activeBook} />
+        )} */}
 
         {isSuccess &&
-          books.map(
-            (data, i) =>
-              data.$id !== activeBook?.$id && (
-                <BookCards setActiveBook={setActiveBook} key={i} {...data} colorScheme={colorScheme} />
-              )
-          )}
+          books.map((data, i) => (
+            <BookCards
+              key={i}
+              {...data}
+              activeBook={activeBook}
+              colorScheme={colorScheme}
+            />
+          ))}
 
         {isLoading && <LoadingSkeleton colorScheme={colorScheme} />}
       </Stack>
