f9efb13750de456cc91eed178d5b272f53b70661
hyperingenious <skbmasale941@gmail.com>
2025-03-18 20:02:37 +0530

    REMOVED unsed shit

--- src/app/components/layout/AppShellLayout.jsx
+++ src/app/components/layout/AppShellLayout.jsx
@@ -1,9 +1,17 @@
 "use client";
 import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import { SignedIn, SignOutButton, useUser } from "@clerk/nextjs";
-import { AppShell, Avatar, Group, Stack, Text, useMantineTheme, useComputedColorScheme } from "@mantine/core";
+import {
+  AppShell,
+  Avatar,
+  Group,
+  Stack,
+  Text,
+  useMantineTheme,
+  useComputedColorScheme,
+} from "@mantine/core";
 import { SignOut } from "@phosphor-icons/react";
-import { Toaster } from "react-hot-toast";
+import  { Toaster } from "react-hot-toast";
 
 import BottomNavigationBar from "../BottomBar";
 import BrandLogo from "../Logo";
@@ -13,7 +21,7 @@ import NavigationRoutes from "./NavRoutes";
 
 import { dark_theme } from "@/app/config/theme";
 import ThemeSwitcher from "./ThemeSwitcher";
-import { memo } from "react";
+import { memo,} from "react";
 import PDFUploadModalProvider from "../PDFUploadModalProvider";
 import OnSignedOutLayout from "./OnSignedOutLayout";
 import CheckDesktopScreen from "./CheckDesktopScreen";
@@ -34,9 +42,18 @@ function AppShellLayout({ children }) {
   const [isNavbarOpen, { toggle: toggleNavbar }] = useDisclosure();
 
   const AppShellHeader = memo(() => (
-    <AppShell.Header bg={colorScheme === "dark" ? "#0f1523" : mantineTheme.colors.gray[0]}>
+    <AppShell.Header
+      bg={colorScheme === "dark" ? "#0f1523" : mantineTheme.colors.gray[0]}
+    >
       <Group justify="space-between" h="100%" px="md">
-        {isTabletScreen && <Avatar src={user?.imageUrl} onClick={toggleNavbar} alt={user?.fullName} size={32} />}
+        {isTabletScreen && (
+          <Avatar
+            src={user?.imageUrl}
+            onClick={toggleNavbar}
+            alt={user?.fullName}
+            size={32}
+          />
+        )}
         <BrandLogo />
         {isTabletScreen && <ThemeSwitcher />}
         {!isTabletScreen && <FeedbackButton open={openFBM} />}
@@ -48,15 +65,15 @@ function AppShellLayout({ children }) {
 
   // Abstraction for subscriptoin model
   const [opened, { open, close }] = useDisclosure(false);
-  // abs for feedback modal 
+  // abs for feedback modal
   const [openedFBM, { open: openFBM, close: closeFBM }] = useDisclosure(false);
+
   return (
     <>
       <OnSignedOutLayout>
         <SharedContent />
       </OnSignedOutLayout>
       <SignedIn>
-
         <SubscriptionModal opened={opened} close={close} />
         <PDFUploadModalProvider />
         <FeedbackModalProvider opened={openedFBM} close={closeFBM} />
@@ -73,7 +90,12 @@ function AppShellLayout({ children }) {
         >
           <Toaster position="bottom-center" reverseOrder={false} />
           <AppShellHeader />
-          <AppShell.Navbar p="md" bg={colorScheme === "dark" ? "#0f1523" : mantineTheme.colors.gray[0]}>
+          <AppShell.Navbar
+            p="md"
+            bg={
+              colorScheme === "dark" ? "#0f1523" : mantineTheme.colors.gray[0]
+            }
+          >
             <Stack gap={0} h="100%" justify="space-between">
               <Stack gap={0}>
                 <UserProfileCard
@@ -81,15 +103,27 @@ function AppShellLayout({ children }) {
                   fullName={user?.fullName}
                   emailAddress={user?.emailAddresses[0]?.emailAddress}
                   imageUrl={user?.imageUrl}
-                  color={colorScheme === "dark" ? "rgb(19, 27, 46)" : mantineTheme.colors.gray[0]}
+                  color={
+                    colorScheme === "dark"
+                      ? "rgb(19, 27, 46)"
+                      : mantineTheme.colors.gray[0]
+                  }
                 />
-                <NavigationRoutes toggle={toggleNavbar} colorScheme={colorScheme} />
+                <NavigationRoutes
+                  toggle={toggleNavbar}
+                  colorScheme={colorScheme}
+                />
               </Stack>
               <Stack gap={0}>
                 <SubscriptionCard
                   open={open}
                   colorScheme={colorScheme}
-                  color={colorScheme === "dark" ? "rgb(19, 27, 46)" : mantineTheme.colors.gray[0]} />
+                  color={
+                    colorScheme === "dark"
+                      ? "rgb(19, 27, 46)"
+                      : mantineTheme.colors.gray[0]
+                  }
+                />
 
                 <Group mb={"md"} justify="space-between" gap={0}>
                   <SignOutButton>
@@ -102,12 +136,24 @@ function AppShellLayout({ children }) {
                         cursor: "pointer",
                       }}
                     >
-                      <Text size="sm" c={colorScheme === "dark" ? dark_theme.main_text_color : "red"} fontWeight={500}>
+                      <Text
+                        size="sm"
+                        c={
+                          colorScheme === "dark"
+                            ? dark_theme.main_text_color
+                            : "red"
+                        }
+                        fontWeight={500}
+                      >
                         Sign out
                       </Text>
                       <SignOut
                         weight="bold"
-                        color={colorScheme === "dark" ? dark_theme.main_text_color : "#fa5252"}
+                        color={
+                          colorScheme === "dark"
+                            ? dark_theme.main_text_color
+                            : "#fa5252"
+                        }
                         size={16}
                       />
                     </Group>
@@ -118,7 +164,9 @@ function AppShellLayout({ children }) {
               </Stack>
             </Stack>
           </AppShell.Navbar>
-          <AppShell.Main style={{ paddingInline: isCompactScreen ? 0 : undefined }}>
+          <AppShell.Main
+            style={{ paddingInline: isCompactScreen ? 0 : undefined }}
+          >
             <CheckDesktopScreen>{children}</CheckDesktopScreen>
           </AppShell.Main>
 
@@ -131,4 +179,3 @@ function AppShellLayout({ children }) {
 }
 
 export default AppShellLayout;
-
