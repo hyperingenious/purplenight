31f726c5b3c6bbe26d6d80dad2b54c769540a06d
hyperingenious <skbmasale941@gmail.com>
2025-03-24 07:01:35 +0530

    FIXED: intern's code

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -14,57 +14,12 @@ import TitleComponent from "@/app/components/TitleComponent";
 import { afacad_flux, spectral } from "@/app/font";
 import { dark_theme } from "@/app/config/theme";
 import { useAuth } from "@clerk/nextjs";
-import { Skeleton } from '@mantine/core';
 
 const TextMarkdown = ({ children }) => (
   <Text fw={400} py="xs" ta="left" size="lg" className={spectral.className}>
     {children}
   </Text>
 );
-
-function Demo() {
-  return (
-    <>
-      <Skeleton height={300} width={785} mb="xl" radius="md" />
-      <Skeleton height={20} width={65} radius="xl" />
-      <Skeleton height={17} mt={6} width={65} radius="md" />
-      <Skeleton height={50} mt={40} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={18} width="40%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="70%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="70%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="30%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-      <Skeleton height={12} mt={12} width="100%" radius="md" />
-    </>
-  );
-}
-
 
 const CodeBlock = ({ children }) => <Code block>{children}</Code>;
 const EmphasizedText = ({ children }) => (
@@ -98,8 +53,8 @@ function ReadBlog() {
   const { id: blog_id } = useParams();
   const colorScheme = useComputedColorScheme();
   const isSmallScreen = useMediaQuery("(max-width:480px)");
-  const isBigScreen = useMediaQuery("(min-width:1200px)");
-  const { getToken } = useAuth();
+  const isBigScreen = useMediaQuery("(min-width:1200px)")
+  const { getToken } = useAuth()
 
   const { data, isLoading, isError } = useQuery({
     queryKey: ["blog", blog_id],
@@ -107,9 +62,12 @@ function ReadBlog() {
   });
 
   if (isLoading) {
-    return <Demo />;
+    return (
+      <Center maw={800} w={isBigScreen ? 800 : "100%"} ref={ref} h="100%">
+        <Loader color={colorScheme === "dark" ? dark_theme.main_text_color : theme.colors.gray[9]} type="dots" />
+      </Center>
+    );
   }
-
   if (isError) {
     return (
       <Center maw={800} w={"100%"} ref={ref} h="100%">
@@ -117,50 +75,23 @@ function ReadBlog() {
       </Center>
     );
   }
-
-  if (!data) {
-    return (
-      <Center maw={800} w={"100%"} ref={ref} h="100%">
-        <Text>No blog data found.</Text>
-      </Center>
-    );
-  }
-
   const { blogData, allBlogsWithBookId } = data;
-
   return (
-    <Stack ref={ref} w={"100%"} miw={300} align="start" maw={800} px="md" mx="auto" gap="lg" mb={isSmallScreen ? 100 : 0}>
+    <Stack ref={ref} w={"100%"} miw={300} align="start" maw={800} px="md" mx="auto" gap="lg" mb={isSmallScreen ? 100 : 0}
+    >
       {/* Blog Image */}
-      <Image 
-        w="100%" 
-        miw={300} 
-        maw={800} 
-        src={blogData?.blog_image || `/images_4_blogs/1.jpg`} 
-        style={{ boxShadow: cardShadows.xs }} 
-        radius="md" 
-        mih={300} 
-        mah={300} 
-      />
+      <Image w="100%" miw={300} maw={800} src={blogData?.blog_image || `/images_4_blogs/1.jpg`} style={{ boxShadow: cardShadows.xs }} radius="md" mih={300} mah={300} />
 
       <Stack gap={0}>
         {/* Book Badge */}
-        <Badge 
-          color={colorScheme === "dark" ? dark_theme.main_text_color : theme.colors.gray[6]} 
-          mt="md" 
-          variant="light" 
-          size="lg" 
-          className={afacad_flux.className} 
-          style={{ boxShadow: cardShadows.xs }}
+        <Badge color={colorScheme === "dark" ? dark_theme.main_text_color : theme.colors.gray[6]} mt="md" variant="light" size="lg" className={afacad_flux.className} style={{ boxShadow: cardShadows.xs }}
         >
           {blogData.books.book_name}
         </Badge>
 
         {/* Author Name */}
-        <Text 
-          c={colorScheme === "dark" ? "#febeb5" : theme.colors.gray[6]} 
-          ta="left" 
-          size="md" 
-          className={afacad_flux.className}
+        <Text
+          c={colorScheme === "dark" ? "#febeb5" : theme.colors.gray[6]} ta="left" size="md" className={afacad_flux.className}
         >
           —{blogData.books?.author || "Unknown"}
         </Text>
